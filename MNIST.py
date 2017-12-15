c215dadb27dab72b8aca21b1d3e6124362982e7c
Amol Agrawal <pfrcks@gmail.com>
2017-12-14 20:49:49 -0500

    Objective based

--- MNIST.py
+++ MNIST.py
@@ -2,10 +2,12 @@ from __future__ import print_function
 import argparse
 import torch
 import sys
+import struct
 import numpy as np
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+import torch.utils.data as data_utils
 from torchvision import datasets, transforms
 from torch.autograd import Variable
 
@@ -34,8 +36,12 @@ torch.manual_seed(args.seed)
 if args.cuda:
     torch.cuda.manual_seed(args.seed)
 
+def read_idx(filename):
+    with open(filename, 'rb') as f:
+        zero, data_type, dims = struct.unpack('>HBB', f.read(4))
+        shape = tuple(struct.unpack('>I', f.read(4))[0] for d in range(dims))
+        return np.fromstring(f.read(), dtype=np.uint8).reshape(shape)
 
-vals = np.zeros((100000, 28, 28))
 
 kwargs = {'num_workers': 1, 'pin_memory': True} if args.cuda else {}
 train_loader = torch.utils.data.DataLoader(
@@ -69,7 +75,7 @@ class Net(nn.Module):
         x = F.relu(self.fc1(x))
         x = F.dropout(x, training=self.training)
         x = self.fc2(x)
-        return F.log_softmax(x)
+        return x
 
 model = Net()
 if args.cuda:
@@ -77,7 +83,25 @@ if args.cuda:
 
 optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
 
-def train(epoch):
+
+def train2(x_train, y_train):
+    model.train()
+    x_train = torch.from_numpy(x_train).type(torch.FloatTensor)
+    y_train = torch.from_numpy(y_train).type(torch.LongTensor)
+    if args.cuda:
+        data, target = x_train.cuda(), y_train.cuda()
+    data, target = Variable(data), Variable(target)
+    optimizer.zero_grad()
+    output = model(data)
+    loss = F.cross_entropy(output, target, reduce=False)
+    losses = loss.cpu().data.numpy()
+    loss = loss.sum()
+    loss.backward()
+    optimizer.step()
+    # print(loss.data)
+    return losses
+
+def train(x_train, y_train):
     model.train()
     for batch_idx, (data, target) in enumerate(train_loader):
         if args.cuda:
@@ -85,13 +109,36 @@ def train(epoch):
         data, target = Variable(data), Variable(target)
         optimizer.zero_grad()
         output = model(data)
-        loss = F.nll_loss(output, target)
+        # criterion = nn.CrossEntropyLoss()
+        loss = F.cross_entropy(output, target, reduce=False)
+        # loss = criterion(output, target, reduce=False)
+        losses[batch_idx*args.batch_size:(batch_idx+1)*args.batch_size] = loss.data.numpy()
+        loss = loss.sum()
         loss.backward()
         optimizer.step()
         if batch_idx % args.log_interval == 0:
             print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                 epoch, batch_idx * len(data), len(train_loader.dataset),
                 100. * batch_idx / len(train_loader), loss.data[0]))
+
+
+def test2(x_test, y_test):
+    model.eval()
+    test_loss = 0
+    correct = 0
+    x_test = torch.from_numpy(x_test).type(torch.FloatTensor)
+    y_test = torch.from_numpy(y_test).type(torch.LongTensor)
+    if args.cuda:
+        data, target = x_test.cuda(), y_test.cuda()
+    data, target = Variable(data, volatile=True), Variable(target, volatile=True)
+    output = model(data)
+    test_loss += F.nll_loss(output, target, size_average=False).data[0] # sum up batch loss
+    pred = output.data.max(1, keepdim=True)[1] # get the index of the max log-probability
+    correct += pred.eq(target.data.view_as(pred)).cpu().sum()
+    test_loss /= 10000
+    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+        test_loss, correct, 10000,
+        100. * correct / 10000))
 
 def test():
     model.eval()
@@ -112,8 +159,38 @@ def test():
         100. * correct / len(test_loader.dataset)))
 
 
-print(train_loader)
-sys.exit()
-for epoch in range(1, args.epochs + 1):
-    train(epoch)
-    test()
+n = 50000
+# losses = torch.Tensor(50000).zero_()
+
+x_train = read_idx('../data/raw/train-images-idx3-ubyte')
+x_test = read_idx('../data/raw/t10k-images-idx3-ubyte')
+y_train = read_idx('../data/raw/train-labels-idx1-ubyte')
+y_test =  read_idx('../data/raw/t10k-labels-idx1-ubyte')
+
+x_train = x_train.reshape(60000, 1, 28, 28)
+x_test = x_test.reshape(10000, 1, 28, 28)
+
+# print(x_train.shape, y_train.shape)
+# print(x_test.shape, y_test.shape)
+# print(torch.Tensor(x_train).size(0))
+# print(torch.Tensor(y_train).size(0))
+
+# train  = data_utils.TensorDataset(x_train, y_train)
+# train_loader = data_utils.DataLoader(train, batch_size=args.batch_size, shuffle=True)
+
+# for val in train_loader:
+    # print(val)
+
+losses = train2(x_train, y_train)
+losses = losses.reshape(losses.shape[0], 1)
+losses = np.column_stack([losses, range(losses.shape[0])])
+
+for epoch in range(10):
+    for i in range(60000/args.batch_size):
+        # ind = np.argsort(losses[:, 0])
+        # losses = losses[ind][::-1]
+        # idx = losses[:, 1][:64]
+        # idx = idx.astype(int)
+        loss_batch = train2(x_train[i*64:(i+1)*64], y_train[i*64:(i+1)*64])
+        # losses[:, 0][:64] = loss_batch
+    test2(x_test, y_test)
