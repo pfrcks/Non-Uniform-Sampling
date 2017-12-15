f8201aa8fa89ceb3da529b6fd4c7bec908565b74
Amol Agrawal <pfrcks@gmail.com>
2017-12-14 21:24:35 -0500

    Objective based WORKS

--- MNIST.py
+++ MNIST.py
@@ -75,7 +75,7 @@ class Net(nn.Module):
         x = F.relu(self.fc1(x))
         x = F.dropout(x, training=self.training)
         x = self.fc2(x)
-        return x
+        return F.log_softmax(x)
 
 model = Net()
 if args.cuda:
@@ -95,31 +95,33 @@ def train2(x_train, y_train):
     output = model(data)
     loss = F.cross_entropy(output, target, reduce=False)
     losses = loss.cpu().data.numpy()
-    loss = loss.sum()
+    # losses = np.zeros(x_train.shape[0])
+    loss = F.nll_loss(output, target)
+    # loss = loss.sum()
     loss.backward()
     optimizer.step()
     # print(loss.data)
     return losses
 
-def train(x_train, y_train):
-    model.train()
-    for batch_idx, (data, target) in enumerate(train_loader):
-        if args.cuda:
-            data, target = data.cuda(), target.cuda()
-        data, target = Variable(data), Variable(target)
-        optimizer.zero_grad()
-        output = model(data)
-        # criterion = nn.CrossEntropyLoss()
-        loss = F.cross_entropy(output, target, reduce=False)
-        # loss = criterion(output, target, reduce=False)
-        losses[batch_idx*args.batch_size:(batch_idx+1)*args.batch_size] = loss.data.numpy()
-        loss = loss.sum()
-        loss.backward()
-        optimizer.step()
-        if batch_idx % args.log_interval == 0:
-            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-                epoch, batch_idx * len(data), len(train_loader.dataset),
-                100. * batch_idx / len(train_loader), loss.data[0]))
+# def train(x_train, y_train):
+    # model.train()
+    # for batch_idx, (data, target) in enumerate(train_loader):
+        # if args.cuda:
+            # data, target = data.cuda(), target.cuda()
+        # data, target = Variable(data), Variable(target)
+        # optimizer.zero_grad()
+        # output = model(data)
+        # # criterion = nn.CrossEntropyLoss()
+        # loss = F.cross_entropy(output, target, reduce=False)
+        # # loss = criterion(output, target, reduce=False)
+        # losses[batch_idx*args.batch_size:(batch_idx+1)*args.batch_size] = loss.data.numpy()
+        # loss = loss.sum()
+        # loss.backward()
+        # optimizer.step()
+        # if batch_idx % args.log_interval == 0:
+            # print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                # epoch, batch_idx * len(data), len(train_loader.dataset),
+                # 100. * batch_idx / len(train_loader), loss.data[0]))
 
 
 def test2(x_test, y_test):
@@ -132,7 +134,7 @@ def test2(x_test, y_test):
         data, target = x_test.cuda(), y_test.cuda()
     data, target = Variable(data, volatile=True), Variable(target, volatile=True)
     output = model(data)
-    test_loss += F.nll_loss(output, target, size_average=False).data[0] # sum up batch loss
+    test_loss += F.cross_entropy(output, target, size_average=False).data[0] # sum up batch loss
     pred = output.data.max(1, keepdim=True)[1] # get the index of the max log-probability
     correct += pred.eq(target.data.view_as(pred)).cpu().sum()
     test_loss /= 10000
@@ -140,26 +142,26 @@ def test2(x_test, y_test):
         test_loss, correct, 10000,
         100. * correct / 10000))
 
-def test():
-    model.eval()
-    test_loss = 0
-    correct = 0
-    for data, target in test_loader:
-        if args.cuda:
-            data, target = data.cuda(), target.cuda()
-        data, target = Variable(data, volatile=True), Variable(target)
-        output = model(data)
-        test_loss += F.nll_loss(output, target, size_average=False).data[0] # sum up batch loss
-        pred = output.data.max(1, keepdim=True)[1] # get the index of the max log-probability
-        correct += pred.eq(target.data.view_as(pred)).cpu().sum()
+# def test():
+    # model.eval()
+    # test_loss = 0
+    # correct = 0
+    # for data, target in test_loader:
+        # if args.cuda:
+            # data, target = data.cuda(), target.cuda()
+        # data, target = Variable(data, volatile=True), Variable(target)
+        # output = model(data)
+        # test_loss += F.nll_loss(output, target, size_average=False).data[0] # sum up batch loss
+        # pred = output.data.max(1, keepdim=True)[1] # get the index of the max log-probability
+        # correct += pred.eq(target.data.view_as(pred)).cpu().sum()
 
-    test_loss /= len(test_loader.dataset)
-    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
-        test_loss, correct, len(test_loader.dataset),
-        100. * correct / len(test_loader.dataset)))
+    # test_loss /= len(test_loader.dataset)
+    # print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+        # test_loss, correct, len(test_loader.dataset),
+        # 100. * correct / len(test_loader.dataset)))
 
 
-n = 50000
+# n = 50000
 # losses = torch.Tensor(50000).zero_()
 
 x_train = read_idx('../data/raw/train-images-idx3-ubyte')
@@ -180,17 +182,18 @@ x_test = x_test.reshape(10000, 1, 28, 28)
 
 # for val in train_loader:
     # print(val)
-
 losses = train2(x_train, y_train)
 losses = losses.reshape(losses.shape[0], 1)
 losses = np.column_stack([losses, range(losses.shape[0])])
 
 for epoch in range(10):
     for i in range(60000/args.batch_size):
-        # ind = np.argsort(losses[:, 0])
-        # losses = losses[ind][::-1]
-        # idx = losses[:, 1][:64]
-        # idx = idx.astype(int)
-        loss_batch = train2(x_train[i*64:(i+1)*64], y_train[i*64:(i+1)*64])
-        # losses[:, 0][:64] = loss_batch
+        ind = np.argsort(losses[:, 0])
+        losses = losses[ind][::-1]
+        idx = losses[:, 1][:64]
+        idx = idx.astype(int)
+        loss_batch = train2(x_train[idx], y_train[idx])
+        # loss_batch = train2(x_train[i*64:(i+1)*64], y_train[i*64:(i+1)*64])
+        # loss_batch = train2(x_train, y_train)
+        losses[:, 0][:64] = loss_batch
     test2(x_test, y_test)
